d5116b6c8c2c0f9aea06f8716d821074ed1082a3
joyce273034 <joyce273034@gmail.com>
2025-01-22 06:15:44 -0800

    fix 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,4 +4,5 @@ import { HomeComponent } from './components/home/home.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: '**', component: HomeComponent },
+  { path: 'portfolio', pathMatch: 'full', component: HomeComponent }
 ];

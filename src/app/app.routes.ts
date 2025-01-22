b6da8e57537f4ea4d567bdc1b495b084faa3f01a
joyce273034 <joyce273034@gmail.com>
2025-01-22 05:49:25 -0800

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-import { ProjectsComponent } from './components/projects/projects.component';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'projects', component: ProjectsComponent },
   { path: '**', component: HomeComponent },
 ];
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,9 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
 @NgModule({
   declarations: [],
   imports: [
@@ -15,20 +13,16 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatListModule,
     MatMenuModule,
     MatCardModule,
-    MatFormFieldModule
   ],
   exports: [
     CommonModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatListModule,
     MatMenuModule,
     MatCardModule,
-    MatFormFieldModule
   ],
 })
 export class MaterialModule {}

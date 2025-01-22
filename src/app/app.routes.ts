d5f1805df0df740e5f8c920d16d661526e6f4529
joyce273034 <joyce273034@gmail.com>
2025-01-22 14:55:22 -0800

    fix i18n build

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: '**', component: HomeComponent },
-  { path: 'portfolio', pathMatch: 'full', component: HomeComponent }
+  // { path: '**', component: HomeComponent },
+  { path: 'portfolio', pathMatch: 'full', component: HomeComponent },
+  { path: '**', redirectTo: '' }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
